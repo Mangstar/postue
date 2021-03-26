2d73c7137180168da203e7335bff9026b0d984c8
Mikhail <MParkhomets@korusconsulting.ru>
2021-03-26 12:06:23 +0300

    Изменения в .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,51 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
     es6: true
   },
   extends: [
     '@vue/standard',
-    'plugin:vue/essential',
-    'plugin:vue/strongly-recommended',
     'plugin:vue/recommended'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
-    semi: ['error', 'always'],
-    indent: ['error', 2],
+    'semi': ['error', 'always'],
     'linebreak-style': ['error', 'unix'],
     'eol-last': ['off'],
     'no-trailing-spaces': 'off',
+    'getter-return': 'warn',
+    'no-setter-return': 'error',
+    'no-dupe-else-if': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'no-extra-semi': 'warn',
+    'no-import-assign': 'error',
+    'accessor-pairs': 'off',
+    'default-case': 'warn',
+    'default-case-last': 'warn',
+    'default-param-last': 'error',
+    'grouped-accessor-pairs': 'warn',
+    'guard-for-in': 'error',
+    'no-else-return': 'error',
+    'no-eq-null': 'error',
+    'no-fallthrough': 'warn',
+    'no-invalid-this': 'warn',
+    'no-magic-numbers': 'warn',
+    'no-useless-concat': 'warn',
+    'radix': 'error',
+    'require-await': 'warn',
+    "camelcase": ["error", { "properties": "always" }],
+    'function-call-argument-newline': ['error', 'consistent'],
+    'function-paren-newline': ['error', 'consistent'],
+    'implicit-arrow-linebreak': ['error', 'beside'],
+    'line-comment-position': ['error', 'above'],
+    'multiline-comment-style': ['error'],
+    'newline-per-chained-call': ['warn', { ignoreChainWithDepth: 2 }],
+    'no-underscore-dangle': 'error',
+
     'vue/max-attributes-per-line': [
       'error',
       {
